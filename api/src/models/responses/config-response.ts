e87d3cc5c0ae9cbd725d3bab820cb888bcafb7ce
geofffaiers <geoff@gfaiers.com>
2025-04-20 23:40:44 +0100

    refactor: simplify ConfigResponse structure and streamline getConfig response

--- api/src/models/responses/config-response.ts
+++ api/src/models/responses/config-response.ts
@@ -1,13 +1,5 @@
-import { Expose } from 'class-transformer';
 import { Project } from '../project';
-import { IsArray, ValidateNested } from 'class-validator';
-import { Type } from 'class-transformer';
 
-export class ConfigResponse {
-
-    @IsArray()
-    @ValidateNested({ each: true })
-    @Type(() => Project)
-    @Expose({ name: 'projects' })
-        projects: Project[] = [];
+export interface ConfigResponse {
+    projects: Project[];
 }
