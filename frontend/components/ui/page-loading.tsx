cf00d5e6ea58d9a8a4792ac88148fbfb4ee9b007
Geoff Faiers <geoff@gfaiers.com>
2025-04-24 07:04:24 +0100

    fix: adjust PageLoading component to ensure full-width display (#94)

--- frontend/components/ui/page-loading.tsx
+++ frontend/components/ui/page-loading.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from 'lucide-react';
 
 export const PageLoading = () => {
     return (
-        <div className='flex flex-col items-center justify-center h-screen bg-background'>
+        <div className='flex flex-col items-center justify-center h-screen bg-background w-full'>
             <Loader2 className='w-16 h-16 animate-spin' />
         </div>
     );
