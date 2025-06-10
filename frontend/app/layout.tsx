d0bb28b0bb9f9466730272862201c28da915e930
Geoff Faiers <geoff@gfaiers.com>
2025-06-10 20:14:27 +0100

    feat: add Google Analytics integration (#119)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import React, { JSX } from 'react';
 import type { Metadata } from 'next';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import './globals.css';
 import { zxcvbnOptions } from '@zxcvbn-ts/core';
 import * as zxcvbnCommonPackage from '@zxcvbn-ts/language-common';
@@ -34,12 +35,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>): JSX.Element {
+    const isProd = process.env.NODE_ENV === 'production';
+    const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
     return (
         <html lang='en' suppressHydrationWarning>
             <head>
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
             </head>
             <body className='antialiased'>
+                {isProd && gaId != null && <GoogleAnalytics gaId={gaId}/>}
                 <ConfigProvider>
                     <ThemeProvider
                         attribute='class'
