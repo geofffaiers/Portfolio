109e6710c3a3d14f2bea95d52c255780ad54ec4a
Geoff Faiers <geoff@gfaiers.com>
2025-07-03 22:59:13 +0100

    fix: update crypto import to use 'node:crypto' for consistency across… (#139)
    * fix: update crypto import to use 'node:crypto' for consistency across services
    - Fix ports for docker-compose to enable local development at the same time
    
    * fix: linting fixes

--- api/src/service/users/logout.ts
+++ api/src/service/users/logout.ts
@@ -1,4 +1,4 @@
-import crypto from 'crypto';
+import crypto from 'node:crypto';
 import { Request, Response } from 'express';
 import { RowDataPacket } from 'mysql2';
 import { pool } from '../../helpers/db';
