4de3da25a6161cb3c59263fd80b135502181ca08
Geoff Faiers <geoff@gfaiers.com>
2025-07-06 19:19:17 +0100

    feat: implement confirm logout functionality in active sessions (#153)
    * feat: implement confirm logout functionality in active sessions
    
    * fix: remove unnecessary JSX type annotation in ConfirmLogout component and reset sessionToLogout state after logout

--- frontend/app/account/use-active-sessions.ts
+++ frontend/app/account/use-active-sessions.ts
@@ -9,15 +9,21 @@ import { useCallback, useEffect, useState } from 'react';
 type UseActiveSessions = {
     loading: boolean;
     sessions: Session[];
-    handleLogOutSession: (session: Session) => void;
+    openConfirmLogout: boolean;
+    handleLogoutSession: (session: Session) => void;
+    setOpenConfirmLogout: (open: boolean) => void;
+    confirmLogout: () => Promise<void>;
+    cancelLogout: () => void;
 };
 
 export function useActiveSessions(): UseActiveSessions {
-    const { authReady } = useAuthContext();
+    const { authReady, setUser } = useAuthContext();
     const { config } = useConfigContext();
     const { displayError } = useToastWrapper();
     const [sessions, setSessions] = useState<Session[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
+    const [openConfirmLogout, setOpenConfirmLogout] = useState<boolean>(false);
+    const [sessionToLogout, setSessionToLogout] = useState<Session | null>(null);
 
     const fetchSessions = useCallback(async () => {
         try {
@@ -51,7 +57,16 @@ export function useActiveSessions(): UseActiveSessions {
         }
     }, [authReady, fetchSessions]);
 
-    const handleLogOutSession = useCallback(async (session: Session) => {
+    const handleLogoutSession = async (session: Session) => {
+        if (session.thisSession) {
+            setSessionToLogout(session);
+            setOpenConfirmLogout(true);
+            return;
+        }
+        await logoutSession(session);
+    };
+
+    const logoutSession = async (session: Session) => {
         try {
             setLoading(true);
             const response = await fetch(`${config.apiUrl}/users/sessions/${session.id}`, {
@@ -64,7 +79,11 @@ export function useActiveSessions(): UseActiveSessions {
             setLoading(true);
             const json: DefaultResponse<{ sessions: Session[] }> = await response.json();
             if (json.success && json.data != null) {
-                setSessions(json.data.sessions);
+                const receivedSessions = json.data.sessions;
+                setSessions(receivedSessions);
+                if (receivedSessions.length === 0 || !receivedSessions.find((s) => s.thisSession)) {
+                    setUser(null);
+                }
             }
         } catch (error: unknown) {
             if (error instanceof Error) {
@@ -75,7 +94,29 @@ export function useActiveSessions(): UseActiveSessions {
         } finally {
             setLoading(false);
         }
-    }, [config.apiUrl, displayError]);
+    };
 
-    return { loading, sessions, handleLogOutSession };
+    const confirmLogout = async () => {
+        if (sessionToLogout == null) {
+            setOpenConfirmLogout(false);
+            return;
+        }
+        await logoutSession(sessionToLogout);
+        setSessionToLogout(null);
+        setOpenConfirmLogout(false);
+    };
+
+    const cancelLogout = () => {
+        setOpenConfirmLogout(false);
+    };
+
+    return {
+        loading,
+        sessions,
+        openConfirmLogout,
+        handleLogoutSession,
+        setOpenConfirmLogout,
+        confirmLogout,
+        cancelLogout,
+    };
 }
